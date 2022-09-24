c2464555af32bb1f21bae3e9c9b29eeab0dc0f72
Caio G. D. Costa <caiogcosta03@gmail.com>
2022-09-24 16:14:40 -0300

    Atividades Python

--- lista de exercícios-45.py
+++
@@ -1,7 +0,0 @@
-#45. Defina uma função soma() que recebe dois números como parâmetros e calcula a soma entre eles.
-n1 = float(input("informe o primeiro número: "))
-n2 = float(input("informe o segundo número: "))
-def soma(n1,n2):
-    s = n1+n2
-    print(f"a soma dos dois valores é: {s}")
-soma(n1, n2)
